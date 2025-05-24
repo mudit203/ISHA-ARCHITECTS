ee6de36c31bcaf9998158df441f69b1294faf00f
Mudit Sharma <115790300+mudit203@users.noreply.github.com>
2025-05-25 01:27:59 +0530

    minor change

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -17,14 +17,14 @@ const Hero = () => {
       {/* Add an overlay to make text more readable */}
       {/* <div className="absolute inset-0 backdrop-blur-[5px]"></div> */}
       
-      <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24">
-        <div className="text-center">
+      <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24 flex flex-col items-center justify-center mt-16">
+        <div className="text-center flex flex-col gap-18">
           <div className='flex justify-center items-center flex-col'>
             <motion.span 
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.8, delay: 0.2 }}
-              className='text-3xl lg:text-8xl font-extrabold'
+              className='text-4xl lg:text-8xl font-extrabold'
             >
               Welcome to
             </motion.span>
@@ -32,7 +32,7 @@ const Hero = () => {
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.8, delay: 0.4 }}
-              className='text-3xl lg:text-8xl font-extrabold bg-yellow-500 p-3 rounded-lg'
+              className='text-4xl lg:text-8xl font-extrabold bg-yellow-500 p-3 rounded-lg'
             >
               ISHA ARCHITECTS
             </motion.span>
@@ -52,7 +52,7 @@ const Hero = () => {
             transition={{ duration: 0.8, delay: 0.8 }}
             className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8"
           >
-            <div className='flex flex-col lg:flex-row gap-10 md:gap-4'>
+            <div className='flex flex-col lg:flex-row md:gap-4'>
             <div className="rounded-md shadow">
               <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-yellow-500 hover:bg-yellow-600 md:py-4 md:text-lg md:px-10">
                 Contact Us
